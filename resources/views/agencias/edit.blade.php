70b2991425c04c4f5f233f831368ebb128c70c2b
Arlin Holguin <arlingholguin@gmail.com>
2024-12-06 11:43:18 -0500

    Ajustes

--- resources/views/agencias/edit.blade.php
+++ resources/views/agencias/edit.blade.php
@@ -1,16 +1,17 @@
 @extends('layout')
 
 @section('main')
-<div class="p-4 bg-blue-500">
-    <h1 clas="text-3xl font-bold">Editar Agencia</h1>
+<div class="p-4">
+    <h1 class="text-3xl font-bold">Editar Agencia</h1>
     <form action="{{ route('agencias.update', $agencia->id) }}" method="POST" class="mt-4">
         @csrf
         @method('PUT')
 
         {{-- Campo Nombre --}}
-        <div class="mb-3">
+        <div class="mb-3 flex flex-col">
             <label for="name" class="form-label">Nombre</label>
             <input 
+			class="h-10 p-2 border border-gray-300 rounded-md"
                 type="text" 
                 class="form-control" 
                 id="name" 
@@ -20,9 +21,10 @@
         </div>
 
         {{-- Campo NIT --}}
-        <div class="mb-3">
+        <div class="mb-3 flex flex-col">
             <label for="nit" class="form-label">NIT</label>
             <input 
+			class="h-10 p-2 border border-gray-300 rounded-md"
                 type="number" 
                 class="form-control" 
                 id="nit" 
@@ -32,19 +34,19 @@
         </div>
 
         {{-- Campo Tipo --}}
-        <div class="mb-3">
+        <div class="mb-3 flex flex-col">
             <label for="type" class="form-label">Tipo</label>
-            <select class="form-select" id="type" name="type" required>
+            <select class="h-10 p-2 border border-gray-300 rounded-md"id="type" name="type" required>
                 <option value="Principal" {{ $agencia->type == 'Principal' ? 'selected' : '' }}>Principal</option>
                 <option value="Sucursal" {{ $agencia->type == 'Sucursal' ? 'selected' : '' }}>Sucursal</option>
             </select>
         </div>
 
         {{-- Botón de Guardar --}}
-        <button type="submit" class="btn btn-primary">Guardar Cambios</button>
+        <button type="submit" class="px-6 py-2 rounded-lg shadow hover:bg-blue-600 bg-blue-900 text-white">Guardar Cambios</button>
 
         {{-- Botón de Cancelar --}}
-        <a href="{{ route('agencias.index') }}" class="btn btn-secondary">Cancelar</a>
+        <a href="{{ route('agencias.index') }}" class="px-6 py-2 rounded-lg shadow hover:!bg-red-500 bg-red-700 text-white">Cancelar</a>
     </form>
 </div>
 @endsection
